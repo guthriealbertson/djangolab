3fc9ceae875fc221ee7081bcefd1317875a28c8d
Guthrie <tui50484@temple.edu>
2022-03-19 14:33:00 +0000

    initial commit

--- mysite/img/urls.py
+++ mysite/img/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
   
+#comment
 urlpatterns = [
     path('', avatarView, name = ''),
     path('success/', success, name = 'success'),
